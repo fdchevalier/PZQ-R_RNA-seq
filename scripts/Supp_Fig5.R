35f2ea21bdc7a422afd326c846f8ba134c273985
fdchevalier <fcheval@txbiomed.org>
2021-08-10 11:48:08 -0500

    Redesign layout of supp. figure 5
    The R script draw the figure following a new layout. In addition the
    new script name has been added.

--- scripts/Supp_Fig5.R
+++ scripts/Supp_Fig5.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
-# Title: Supp. Fig. X2
-# Version: 0.0
+# Title: Supp_Fig5.R
+# Version: 0.1
 # Author: Frédéric CHEVALIER <fcheval@txbiomed.org>
 # Created in: 2021-06-06
-# Modified in:
+# Modified in: 2021-08-10
 
 
 
@@ -19,6 +19,7 @@
 # Versions #
 #==========#
 
+# v0.1 - 2021-08-10: rename script / update graph path / redesign graph layout
 # v0.0 - 2021-06-06: creation
 
 
@@ -118,10 +119,28 @@ cat("Generating graphs...\n")
 
 if (! dir.exists(graph_fd)) { dir.create(graph_fd) }
 
+pdf(paste0(graph_fd, "Supp. Fig. 5.pdf"), width = 8, heigh = 3 * nrow(mycomp), useDingbats=TRUE)
 
-pdf(paste0(graph_fd, "Supp. Fig. X2.pdf"), width = 8, heigh = 4 * nrow(mycomp), useDingbats=TRUE)
+mycex <- 1.5
 
-layout(matrix(c(1:(nrow(mycomp) * 2)), ncol=2, byrow = FALSE))
+# Layout matrix
+## Core matrix
+mymat <- matrix(c(1:(nrow(mycomp) * 2)), ncol=2, byrow = FALSE)
+
+## Add rows
+mymat <- rbind(max(mymat) + 1:2, mymat)
+mymat <- rbind(mymat, rep(max(mymat) + 1, 2))
+mymat <- rbind(mymat, rep(max(mymat) + 1, 2))
+
+## Add columns
+mymat <- cbind(c(0, rep(max(mymat) + 1, nrow(mycomp)), 0, 0), mymat)
+mymat <- cbind(mymat, c(0, max(mymat) + 1:nrow(mycomp), 0, 0))
+
+## Create layout
+layout(mymat, widths = c(0.1, 1, 1, 0.5), heights = c(0.1, rep(1, nrow(mycomp)), 0.1, 0.2))
+
+# Define margins
+par(mar = c(2, 2, 1, 1))
 
 for (t in 1:length(mytypes)) {
 
@@ -130,7 +149,6 @@ for (t in 1:length(mytypes)) {
     for (j in 1:nrow(mycomp)) {
         
         mynm    <- paste0(c(mytype.tmp, mycomp[j,]), collapse="-")
-        mytitle <- sapply(mycomp[j,], function(x) myconditions[ myconditions[,1] == x, 2]) %>% paste0(., collapse=" vs. ")
         
         # Loading data
         myDE.tb <- myDE_all[[t]][[mynm]]
@@ -150,7 +168,11 @@ for (t in 1:length(mytypes)) {
             myclr[[i]][ grepl(paste0(mygenes[[i-1]], collapse="|"), rownames(myresults)) ] <- TRUE 
         }
 
-        plot(log2(myresults$PostFC), myresults$PPDE, xlab="Fold change (log2)", ylab="Posterior probability", pch=19, col=clr.vec[1], main=paste(tools::toTitleCase(mytype.tmp), "expression -", mytitle))
+        # Axis
+        if (t == 2) { myyax <- "n" } else { myyax <- "s" }
+        if (j != nrow(mycomp)) { myxax <- "n" } else { myxax <- "s" }
+
+        plot(log2(myresults$PostFC), myresults$PPDE, xlab="", ylab="", pch=19, col=clr.vec[1], xaxt=myxax, yaxt=myyax)
         for (i in 1:length(myclr)) {
             points(log2(myresults$PostFC)[myclr[[i]]], myresults$PPDE[myclr[[i]]], pch=19, col=clr.vec[i+1])
         }
@@ -159,14 +181,40 @@ for (t in 1:length(mytypes)) {
         abline(v=1,    col=clr.ln, lty=2)
         abline(v=-1,   col=clr.ln, lty=2)
 
-        # legend("bottomleft", c("Genes under\nQTL chr. 3", expression(italic("Sm.TRPM"["PZQ"]))), col=clr.vec[3:length(clr.vec)], pch=19, bty="n")
-        legend("bottomleft", c("QTL chr. 3", expression(italic("Sm.TRPM"["PZQ"]))), col=clr.vec[3:length(clr.vec)], pch=19, bty="n")
-            
     }
     
-    mtext(paste0(LETTERS[t], "."), side=3, line=2, at=line2user(par("mar")[2],2), cex=par("cex")*2, adj=0)
 }
 
+# Redefine margins
+par(mar = c(0, 2, 0, 1))
 
+# Titles
+for (i in mytypes) {
+    plot.new()
+    text(0.5, 0.5, labels = paste(tools::toTitleCase(i), "expression"), font = 2, cex = mycex * 1.5)
+}
+
+# X axis
+plot.new()
+text(0.5, 0.5, labels = "Fold change (log2)", cex = mycex)
+
+# Legend
+plot.new()
+myorder <- matrix(1:length(clr.vec), ncol = 2, byrow = TRUE)
+legend("center", c("Non significant differential expression", "Significant differential expression", "Gene / isoform expressed under QTL chr. 3", expression(italic(Sm.TRPM[PZQ]) ~ expression))[myorder], col = clr.vec[myorder], pch = 19, bty = "n", ncol = 2)
+
+# Redefine margins
+par(mar = rep(0, 4))
+
+# Y axis
+plot.new()
+text(0.5, 0.5, labels = "Posterior probability", cex = mycex, srt = 90)
+
+# Comparison titles
+for(j in 1:nrow(mycomp)) {
+    plot.new()
+    mytitle <- sapply(mycomp[j,], function(x) myconditions[ myconditions[,1] == x, 2]) %>% paste0(., collapse="\nvs.\n")
+    text(0.5, 0.5, labels = mytitle, cex = mycex, font = 2)
+}
 
 dev.off()
